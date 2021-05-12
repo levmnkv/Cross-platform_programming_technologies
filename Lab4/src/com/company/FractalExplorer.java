65b536f9f2489a30527f95ff049ba462c3588310
Lev Minakov <69292332+levmnkv@users.noreply.github.com>
2021-05-12 14:25:09 +0300

    Добавил 5 лабу

--- Lab4/src/com/company/FractalExplorer.java
+++ Lab4/src/com/company/FractalExplorer.java
@@ -1,70 +1,129 @@
 package com.company;
 
 import javax.swing.*;
+import java.io.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.awt.geom.Rectangle2D;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileFilter;
 
 public class FractalExplorer {
-
     private int SCREEN_SIZE;
     private JImageDisplay display;
-    private FractalGenerator fractalGenerator;
-    private Rectangle2D.Double rectangle2d;
+    private FractalGenerator fractal;
+    private Rectangle2D.Double range;
 
-    public FractalExplorer(int SCREEN_SIZE){
-        this.SCREEN_SIZE = SCREEN_SIZE;
-        this.fractalGenerator = new Mandelbrot();
-        this.rectangle2d = new Rectangle2D.Double(0,0,0,0);
-        fractalGenerator.getInitialRange(this.rectangle2d);
+    public FractalExplorer(int displaySize) {
+        this.SCREEN_SIZE = displaySize;
+        this.fractal = new Mandelbrot();
+        this.range = new Rectangle2D.Double(0, 0, 0, 0);
+        fractal.getInitialRange(this.range);
     }
 
-    public void drawFractal(){
-        for(int i = 0; i < SCREEN_SIZE; i++){
-            for(int j = 0; j < SCREEN_SIZE; j++){
-                double xCoord = FractalGenerator.getCoord(rectangle2d.x,rectangle2d.x + rectangle2d.width, SCREEN_SIZE, i);
-                double yCoord = FractalGenerator.getCoord(rectangle2d.y,rectangle2d.y + rectangle2d.height, SCREEN_SIZE, j);
-                int iter = fractalGenerator.numIterations(xCoord,yCoord);
-                if (iter == -1)display.drawPixel(i,j,0);
+    public void createAndShowGUI() {
+        Mandelbrot mandelbrot = new Mandelbrot();
+        Tricorn tricorn = new Tricorn();
+        BurningShip burningShip = new BurningShip();
+
+        JFrame frame = new JFrame("Fractal Explorer");
+
+        JPanel northpanel = new JPanel();
+        JPanel southpanel = new JPanel();
+
+        JLabel label = new JLabel("Fractal");
+
+        //String[] fractals={mandelbrot, tricorn, burningShip};
+        JComboBox comboBox = new JComboBox();
+        comboBox.addItem(mandelbrot);
+        comboBox.addItem(tricorn);
+        comboBox.addItem(burningShip);
+
+        display = new JImageDisplay(SCREEN_SIZE, SCREEN_SIZE);
+
+        JButton reset = new JButton("reset");
+        JButton save = new JButton("save");
+
+        //обработчик комбобокса
+        comboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JComboBox am = (JComboBox) e.getSource();
+                fractal = (FractalGenerator) am.getSelectedItem();
+            }
+        });
+        //
+
+        //обработчик кнопки reset
+        reset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                fractal.getInitialRange(range);
+                drawFractal();
+            }
+        });
+        //
+
+        //обработчик кнопки save
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JButton button = (JButton) e.getSource();
+                JFileChooser fileChooser = new JFileChooser();
+                FileFilter filter = new FileNameExtensionFilter("PNG Images", "png");
+                fileChooser.setFileFilter(filter);
+                fileChooser.setAcceptAllFileFilterUsed(false);
+                if (fileChooser.showSaveDialog(button.getParent()) == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        ImageIO.write(display.getBufferedImage(), "png", fileChooser.getSelectedFile());
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(button.getParent(), ex.getMessage(),
+                                "Cannot Save Image", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+        });
+        //
+
+        //рисование фрактала
+        display.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                double xCoord = FractalGenerator.getCoord(range.x, range.x + range.width, SCREEN_SIZE, x);
+                double yCoord = FractalGenerator.getCoord(range.y, range.y + range.height, SCREEN_SIZE, y);
+                fractal.recenterAndZoomRange(range, xCoord, yCoord, 0.5);
+                drawFractal();
+            }
+        });
+        northpanel.add(label);
+        northpanel.add(comboBox);
+        southpanel.add(save);
+        southpanel.add(reset);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(display, BorderLayout.CENTER);
+
+        frame.add(southpanel, BorderLayout.SOUTH);
+        frame.add(northpanel, BorderLayout.NORTH);
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+
+    public void drawFractal() {
+        for (int i = 0; i < SCREEN_SIZE; i++) {
+            for (int j = 0; j < SCREEN_SIZE; j++) {
+                double xCoord = FractalGenerator.getCoord(range.x, range.x + range.width, SCREEN_SIZE, i);
+                double yCoord = FractalGenerator.getCoord(range.y, range.y + range.height, SCREEN_SIZE, j);
+                int iter = fractal.numIterations(xCoord, yCoord);
+                if (iter == -1) display.drawPixel(i, j, 0);
                 else {
-                    float hue = 0.7f + (float)iter / 200f;
-                    display.drawPixel(i,j, Color.HSBtoRGB(hue,1f,1f));
+                    float hue = 0.7f + (float) iter / 200f;
+                    display.drawPixel(i, j, Color.HSBtoRGB(hue, 1f, 1f));
                 }
             }
         }
         display.repaint();
-    }
-
-    public void createAndShowGUI(){
-        JFrame jFrame = new JFrame("Fractal Explorer");
-        display = new JImageDisplay(SCREEN_SIZE, SCREEN_SIZE);
-        JButton btn_reset = new JButton("Reset");
-        btn_reset.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                fractalGenerator.getInitialRange(rectangle2d);
-                drawFractal();
-            }
-        });
-        display.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int x = e.getX();
-                int y = e.getY();
-                double xCoord = FractalGenerator.getCoord(rectangle2d.x, rectangle2d.x + rectangle2d.width, SCREEN_SIZE, x);
-                double yCoord = FractalGenerator.getCoord(rectangle2d.y, rectangle2d.y + rectangle2d.height, SCREEN_SIZE, y);
-                fractalGenerator.recenterAndZoomRange(rectangle2d, xCoord, yCoord, 0.5);
-                drawFractal();
-            }
-        });
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.add(display,BorderLayout.CENTER);
-        jFrame.add(btn_reset,BorderLayout.SOUTH);
-        jFrame.pack();
-        jFrame.setVisible(true);
-        jFrame.setResizable(false);
     }
 }
--- Lab4/src/com/company/Tricorn.java
+++ Lab4/src/com/company/Tricorn.java
@@ -3,17 +3,17 @@ package com.company;
 
 import java.awt.geom.Rectangle2D;
 
-class Mandelbrot extends FractalGenerator{
+class Tricorn extends FractalGenerator{
     public String toString(){
-        return "Mandelbrot";
+        return "Tricorn";
     }
 
     @Override
     public void getInitialRange(Rectangle2D.Double range) {
         range.x = -2.0;
-        range.y = -1.5;
-        range.height = 3;
-        range.width = 3;
+        range.y = -2.0;
+        range.height = 4;
+        range.width = 4;
     }
 
     public static final int MAX_ITERATIONS = 2000;
@@ -23,8 +23,8 @@ class Mandelbrot extends FractalGenerator{
         int iteration = 0;
         double x1 = 0, y1 = 0, _x1 = 0, _y1 = 0;
         while (x1*x1 - y1*y1 < 4 && iteration < MAX_ITERATIONS){
-            _x1 = x1*x1 - y1*y1 + x;
-            _y1 = 2*x1*y1 + y;
+            _x1 = x1 * x1 - y1 * y1 + x;
+            _y1 = -2 * x1 * y1 + y;
             x1 = _x1;
             y1 = _y1;
             iteration++;
