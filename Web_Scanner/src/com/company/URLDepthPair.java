d1949c1a46f63c4e48e1d24e9e531aedd4c99ac2
Lev Minakov <69292332+levmnkv@users.noreply.github.com>
2021-05-18 08:37:46 +0300

    Добавил 7 лабу

---
+++ Web_Scanner/src/com/company/URLDepthPair.java
@@ -0,0 +1,29 @@
+package com.company;
+
+class URLDepthPair {
+    private String url;
+    private int depth;
+    private int visited;
+
+    public URLDepthPair(String url_, int depth_) {
+        url = url_;
+        depth = depth_;
+        visited = 1;
+    }
+
+    public String getURL() {
+        return url;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void incrementVisited() {
+        visited++;
+    }
+
+    public String toString() {
+        return "<URL href=\"" + url + "\" visited=\"" + visited + "\" depth=\"" + depth + "\" \\>";
+    }
+}
